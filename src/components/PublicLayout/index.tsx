8299e2fb30fba2c6e145d1304488b54a0363da6f
Felipe <feferreira.sf@gmail.com>
2023-04-29 18:50:33 -0300

    style: added page transition effect

--- src/components/PublicLayout/index.tsx
+++ src/components/PublicLayout/index.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from "react";
 import Head from "next/head";
 import Image from "next/image";
+import { easeInOut, motion } from "framer-motion";
 import HeroImage from "../../assets/login-register-hero.svg";
 
 interface LayoutProps {
@@ -17,7 +18,17 @@ export default function PublicLayout({ children, pageTitle }: LayoutProps) {
           <meta name='robots' content='noindex' />
         </Head>
       </>
-      <main className='flex h-screen bg-body p-6'>
+      <motion.main
+        initial={{ x: 300, opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        exit={{ x: 300, opacity: 0 }}
+        transition={{
+          type: "spring",
+          stiffness: 260,
+          damping: 20,
+        }}
+        className='flex h-screen bg-body p-6'
+      >
         <div className='m-auto grid gap-x-10 lg:grid-cols-2'>
           <Image
             src={HeroImage}
@@ -28,7 +39,7 @@ export default function PublicLayout({ children, pageTitle }: LayoutProps) {
           />
           <div>{children}</div>
         </div>
-      </main>
+      </motion.main>
     </div>
   );
 }
