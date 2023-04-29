9b9cd1f10e1c8ee3e0b590d8fb352e6ff6327445
Felipe <feferreira.sf@gmail.com>
2023-04-29 18:50:49 -0300

    feat: framer motion context wrapper

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,18 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
+import { AnimatePresence } from "framer-motion";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <SessionProvider session={pageProps.session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <AnimatePresence
+      mode='wait'
+      initial={false}
+      onExitComplete={() => window.scrollTo(0, 0)}
+    >
+      <SessionProvider session={pageProps.session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </AnimatePresence>
   );
 }
