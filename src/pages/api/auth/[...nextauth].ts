8c21e566c22a75f063741d808fb08c0ca1d4b9ae
Felipe <feferreira.sf@gmail.com>
2023-04-30 14:04:42 -0300

    feat: credentials provider

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,8 +1,15 @@
 import NextAuth, { AuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { prisma } from "@/lib/prisma";
-
+import crypto from "crypto";
+import { api } from "@/lib/axios";
+interface PasswordProps {
+  candidatePassword: string;
+  hash: string;
+  salt: string;
+}
 export const authOptions: AuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
@@ -10,7 +17,63 @@ export const authOptions: AuthOptions = {
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
+    CredentialsProvider({
+      id: "credentials",
+      name: "credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "jsmith@test.com",
+        },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        const checkIfUserAlreadyExists = async (email: string) => {
+          return await prisma.user.findUnique({
+            where: {
+              email,
+            },
+          });
+        };
+        // Check if user already exists
+        const isUser = await checkIfUserAlreadyExists(
+          credentials?.email as string
+        );
+        if (!isUser) {
+          throw new Error("Nenhum usuário registrado com esse email");
+        }
+        // Check Password
+        const verifyPassword = ({
+          candidatePassword,
+          hash,
+          salt,
+        }: PasswordProps) => {
+          const candidateHash = crypto
+            .pbkdf2Sync(candidatePassword, salt, 1000, 64, "sha512")
+            .toString("hex");
+
+          const result = candidateHash === hash;
+
+          return result;
+        };
+        const correctPassword = verifyPassword({
+          candidatePassword: credentials?.password as string,
+          hash: isUser.password as string,
+          salt: isUser.salt as string,
+        });
+
+        if (!correctPassword || isUser.email !== credentials?.email) {
+          throw new Error("Senha incorreta");
+        }
+        return isUser;
+      },
+    }),
   ],
+  secret: process.env.NEXT_AUTH_SECRET,
+  session: {
+    strategy: "jwt",
+  },
 };
 
 export default NextAuth(authOptions);
