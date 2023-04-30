c28a8693211b2eeef5e81713e637d6dbcfb94c6b
Felipe <feferreira.sf@gmail.com>
2023-04-30 17:45:33 -0300

    feat: test some protected methods for api routes

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,24 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./auth/[...nextauth]";
 
 type Data = {
-  name: string
-}
+  message: string;
+};
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const session = await getServerSession(req, res, authOptions);
+  console.log(session);
+  if (!session) {
+    return res
+      .status(500)
+      .send({
+        message: "Você precisa estar cadastrado para ver esse conteúdo",
+      });
+  }
+  res.status(200).json({ message: "John Doe" });
 }
