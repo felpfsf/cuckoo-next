dcd4ce204bf69f8256594d17e9bdeee8a92c1fc3
Felipe <feferreira.sf@gmail.com>
2023-04-30 20:23:51 -0300

    feat: added deploy url

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,8 @@
 import axios from "axios";
-
+// https://cuckoo-next.vercel.app/
+// http://localhost:3000
+const LOCAL_URL = process.env.NEXT_LOCAL_URL;
+const BASE_URL = process.env.NEXT_BASE_URL;
 export const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: LOCAL_URL || BASE_URL,
 });
