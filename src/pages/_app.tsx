e855de7cfbcbbc53f09eff38b066d98653a1fee5
Felipe <feferreira.sf@gmail.com>
2023-05-01 14:48:05 -0300

    feat: added loading status with nprogress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,20 @@
-import "@/styles/globals.css";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
 import { AnimatePresence } from "framer-motion";
+import nProgress from "nprogress";
+import "@/styles/globals.css";
+import "@/styles/nprogress.css";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  nProgress.configure({ speed: 500 });
+  useEffect(() => {
+    router.events.on("routeChangeStart", () => nProgress.start());
+    router.events.on("routeChangeComplete", () => nProgress.done());
+    router.events.on("routeChangeError", () => nProgress.done());
+  }, [router.events]);
   return (
     <AnimatePresence
       mode='wait'
