64d60179902d02411020a1e46878d49f7336c2e2
Felipe <feferreira.sf@gmail.com>
2023-05-04 17:52:38 -0300

    feat: each card has it's own link to the it's page

--- src/components/PostCard/index.tsx
+++ src/components/PostCard/index.tsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
+import Link from "next/link";
 import Image from "next/image";
 import { FaRegComment } from "react-icons/fa";
 import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
-import AvatarMockup from "../../assets/avatar_mockup.jpg";
+import AvatarMockup from "../../assets/avatar_mockup_2.png";
 
 interface PostProps {
   id: string;
@@ -10,7 +11,7 @@ interface PostProps {
   author: { name: string; email: string; image: string };
 }
 
-export default function PostCard({ author, content}: PostProps) {
+export default function PostCard({ author, content, id }: PostProps) {
   const [like, setLike] = useState(false);
   const handleLike = () => {
     setLike((prev) => !prev);
@@ -23,61 +24,61 @@ export default function PostCard({ author, content}: PostProps) {
   // </div>
   return (
     // <article className='grid w-full grid-flow-col grid-rows-3 gap-2 border-t border-gray-800 px-4 py-2'>
-    <article className='flex w-full items-start gap-4 border-t border-gray-800 px-4 pt-2'>
-      <figure className='col-span-1 row-span-2 mt-2 flex w-12 flex-col items-center'>
-        <div className='flex h-12 w-12 items-center justify-center overflow-hidden rounded-full bg-fuchsia-900'>
-          <Image
-            src={author.image ? author.image : AvatarMockup}
-            alt='Avatar do usuário'
-            width={48}
-            height={48}
-            className='h-full w-full object-cover'
-          />
-        </div>
-        {/* <figcaption>
+    <Link href={`post/${id}`}>
+      <article className='flex w-full items-start gap-4 border-t border-gray-800 px-4 pt-2'>
+        <figure className='col-span-1 row-span-2 mt-2 flex w-12 flex-col items-center'>
+          <div className='flex h-12 w-12 items-center justify-center overflow-hidden rounded-full bg-fuchsia-900'>
+            <Image
+              src={author.image ? author.image : AvatarMockup}
+              alt='Avatar do usuário'
+              width={48}
+              height={48}
+              className='h-full w-full object-cover'
+            />
+          </div>
+          {/* <figcaption>
           <div id='user-name' className='max-w-[48px]'>
             <p className='break-words text-sm font-semibold'>UsuárioUsuário</p>
           </div>
         </figcaption> */}
-      </figure>
-      <main
-        id='content'
-        className='col-span-2 row-span-3 flex flex-col justify-between'
-      >
-        <div id='user-name' className='pt-2'>
-          <p className='break-words text-sm font-semibold'>{author.name}</p>
-        </div>
-        <div id='post-content' className=''>
-          <p className='text-sm lg:text-base'>
-            {content}
-          </p>
-        </div>
-        <div id='post-actions' className='pt-4'>
-          <nav role='navigation'>
-            <ul className='flex gap-12'>
-              <li className=''>
-                <button
-                  aria-label='Comentar'
-                  className='flex items-center gap-2'
-                >
-                  <FaRegComment />
-                  Comentar
-                </button>
-              </li>
-              <li className=''>
-                <button
-                  aria-label='Curtir'
-                  className='flex items-center gap-2'
-                  onClick={handleLike}
-                >
-                  {like ? <AiFillHeart /> : <AiOutlineHeart />}
-                  Like
-                </button>
-              </li>
-            </ul>
-          </nav>
-        </div>
-      </main>
-    </article>
+        </figure>
+        <main
+          id='content'
+          className='col-span-2 row-span-3 flex flex-col justify-between'
+        >
+          <div id='user-name' className='pt-2'>
+            <p className='break-words text-sm font-semibold'>{author.name}</p>
+          </div>
+          <div id='post-content' className=''>
+            <p className='text-sm lg:text-base'>{content}</p>
+          </div>
+          <div id='post-actions' className='pt-4'>
+            <nav role='navigation'>
+              <ul className='flex gap-12'>
+                <li className=''>
+                  <button
+                    aria-label='Comentar'
+                    className='flex items-center gap-2'
+                  >
+                    <FaRegComment />
+                    Comentar
+                  </button>
+                </li>
+                <li className=''>
+                  <button
+                    aria-label='Curtir'
+                    className='flex items-center gap-2'
+                    onClick={handleLike}
+                  >
+                    {like ? <AiFillHeart /> : <AiOutlineHeart />}
+                    Like
+                  </button>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </main>
+      </article>
+    </Link>
   );
 }
