d655335e566933afb6bc988a005e59f10cd67e4c
Felipe <feferreira.sf@gmail.com>
2023-05-03 21:17:52 -0300

    feat: post fetching and post card dynamic data

--- src/components/PostCard/index.tsx
+++ src/components/PostCard/index.tsx
@@ -5,7 +5,13 @@ import { FaRegComment } from "react-icons/fa";
 import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
 import { useState } from "react";
 
-export default function PostCard() {
+interface PostProps {
+  id: string;
+  content: string;
+  author: { name: string; email: string; image: string };
+}
+
+export default function PostCard({ author, content, id }: PostProps) {
   const [like, setLike] = useState(false);
   const handleLike = () => {
     setLike((prev) => !prev);
@@ -17,12 +23,12 @@ export default function PostCard() {
   //   <div class="row-span-2 col-span-2 ...">03</div>
   // </div>
   return (
-    // <article className='flex w-full items-start gap-4 border-t border-gray-800 px-4 pt-2'>
-    <article className='grid w-full grid-flow-col grid-rows-3 gap-2 px-4 py-2 border-t border-gray-800'>
-      <figure className='col-span-1 row-span-3 mt-2 flex flex-col items-center'>
+    // <article className='grid w-full grid-flow-col grid-rows-3 gap-2 border-t border-gray-800 px-4 py-2'>
+    <article className='flex w-full items-start gap-4 border-t border-gray-800 px-4 pt-2'>
+      <figure className='col-span-1 row-span-2 mt-2 flex w-12 flex-col items-center'>
         <div className='flex h-12 w-12 items-center justify-center overflow-hidden rounded-full bg-fuchsia-900'>
           <Image
-            src={AvatarMockup}
+            src={author.image ? author.image : AvatarMockup}
             alt='Avatar do usuário'
             className='h-full w-full object-cover'
           />
@@ -38,15 +44,11 @@ export default function PostCard() {
         className='col-span-2 row-span-3 flex flex-col justify-between'
       >
         <div id='user-name' className='pt-2'>
-          <p className='break-words text-sm font-semibold'>UsuárioUsuário</p>
+          <p className='break-words text-sm font-semibold'>{author.name}</p>
         </div>
         <div id='post-content' className=''>
           <p className='text-sm lg:text-base'>
-            <Balancer>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum
-              blanditiis a excepturi iure nostrum facilis nisi minus? Sint rerum
-              nihil corporis nostrum maiores. Maiores!
-            </Balancer>
+            {content}
           </p>
         </div>
         <div id='post-actions' className='pt-4'>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,42 @@
-import { useRouter } from "next/router";
-import { useSession } from "next-auth/react";
+import { GetStaticProps } from "next";
+import { prisma } from "@/lib/prisma";
 import MainLayout from "@/components/MainLayout";
 import PostCard from "@/components/PostCard";
 
-function Home() {
-  const router = useRouter();
-  const { data: session, status } = useSession();
-  // if (status === "unauthenticated") {
-  //   router.push("/auth/login");
-  // }
+interface PostProps {
+  id: string;
+  content: string;
+  author: { name: string; email: string; image: string };
+}
 
+interface FeedProps {
+  feed: PostProps[];
+}
+
+function Home({ feed }: FeedProps) {
   return (
     <MainLayout pageTitle='Página Principal - Cuckoo'>
-      <div className="flex flex-col">
-        <PostCard />
-        <PostCard />
-        <PostCard />
-        <PostCard />
-        <PostCard />
-        <PostCard />
-        <PostCard />
-        <PostCard />
-        <PostCard />
-        <PostCard />
-        <PostCard />
+      <h1 className='text-xl font-semibold'>Página Inicial</h1>
+      <div className='flex flex-col'>
+        {feed.map((post) => (
+          <PostCard key={post.id} {...post} />
+        ))}
       </div>
     </MainLayout>
   );
 }
 
 export default Home;
+
+export const getStaticProps: GetStaticProps = async () => {
+  const feed = await prisma.post.findMany({
+    include: {
+      author: {
+        select: { name: true, email: true, image: true },
+      },
+    },
+  });
+  return {
+    props: { feed: JSON.parse(JSON.stringify(feed)) },
+  };
+};
