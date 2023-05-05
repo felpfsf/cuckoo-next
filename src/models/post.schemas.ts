c7346be76e1d1de6514afb00352c8eff2ab99589
Felipe <feferreira.sf@gmail.com>
2023-05-05 17:08:03 -0300

    feat: added comment schema

--- src/models/post.schemas.ts
+++ src/models/post.schemas.ts
@@ -1,7 +1,19 @@
-import z from 'zod'
+import z from "zod";
 
 export const postSchema = z.object({
-  content:z.string().min(1,'Escreva qualquer coisa').max(300, 'Máximo de 300 caracteres')
-})
+  content: z
+    .string()
+    .min(1, "Escreva qualquer coisa")
+    .max(300, "Máximo de 300 caracteres"),
+});
 
-export type PostInputProps = z.infer<typeof postSchema>
+export type PostInputProps = z.infer<typeof postSchema>;
+
+export const commentPostSchema = z.object({
+  content: z
+    .string()
+    .min(1, "Escreva qualquer coisa")
+    .max(300, "Máximo de 300 caracteres"),
+});
+
+export type CommentPostInputProps = z.infer<typeof commentPostSchema>;
