2279e448ef9372a40cf11c8bbdb16e0f21ab5a2f
Felipe <feferreira.sf@gmail.com>
2023-05-05 12:55:03 -0300

    fix: default type for session to include id

--- src/@types/next-auth.d.ts
+++ src/@types/next-auth.d.ts
@@ -1,11 +1,7 @@
 import NextAuth from "next-auth";
+import { DefaultSession } from "next-auth";
 
 declare module "next-auth" {
-  // interface Session {
-  //   user: {
-  //     id: string;
-  //   } & DefaultSession["user"];
-  // }
   interface Session {
     user: {
       id: string;
